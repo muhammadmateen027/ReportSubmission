8ef6db3869485973b72254176eb63e9e2f59fc02
Mateen <muhammadmateen027@gmail.com>
2019-03-27 14:36:53 +0800

    Class updated

--- RssDateLoader/src/main/java/org/sunway/rssdateloader/storebinder/StoreBinderMain.java
+++ RssDateLoader/src/main/java/org/sunway/rssdateloader/storebinder/StoreBinderMain.java
@@ -5,10 +5,43 @@
  */
 package org.sunway.rssdateloader.storebinder;
 
+import org.joget.apps.form.lib.WorkflowFormBinder;
+import org.joget.apps.form.model.Element;
+import org.joget.apps.form.model.FormData;
+import org.joget.apps.form.model.FormRowSet;
+
 /**
  *
  * @author kirthikan
  */
-public class StoreBinderMain {
-    
+public class StoreBinderMain extends WorkflowFormBinder {
+
+    public String getName() {
+        return "ESCA - CustomStoreBinder";
+    }
+
+    public String getVersion() {
+        return "1.0";
+    }
+
+    public String getDescription() {
+        return "ESCA - CustomStoreBinder";
+    }
+
+    public String getLabel() {
+        return getName();
+    }
+
+    public String getClassName() {
+        return getClass().getName();
+    }
+
+    public String getPropertyOptions() {
+        return "";
+    }
+
+    @Override
+    public FormRowSet store(Element element, FormRowSet rowSet, FormData formData) {
+        return null;
+    }
 }
