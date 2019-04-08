4f08fbe0a966722451ff02fdcc27e94cc6e7fbc2
Mateen <muhammadmateen027@gmail.com>
2019-04-08 11:15:46 +0800

    New request has been submitted to Manager

--- RssDateLoader/src/main/java/org/sunway/rssdateloader/storebinder/RequestForm.java
+++ RssDateLoader/src/main/java/org/sunway/rssdateloader/storebinder/RequestForm.java
@@ -6,6 +6,7 @@
 package org.sunway.rssdateloader.storebinder;
 
 import java.sql.ResultSet;
+import org.joget.apps.form.model.FormData;
 import org.joget.apps.form.model.FormRow;
 import org.joget.apps.form.model.FormRowSet;
 import org.sunway.rssdateloader.databases.QueryHandler;
@@ -21,18 +22,24 @@ public class RequestForm implements QueryHandlerInterface {
 
     private FormRowSet rowSet;
     private QueryHandler qh;
+    private FormData formData;
+    public OnCallBack onCallBack;
 
-    public RequestForm(FormRowSet rowSet) {
+    public RequestForm(FormData formData, FormRowSet rowSet, OnCallBack aClass) {
         this.rowSet = rowSet;
+        this.formData = formData;
         this.qh = new QueryHandler(this);
+        this.onCallBack = aClass;
     }
 
-    public void performAction() {
+    public void performAction(String elemId) {
         FormRow row = rowSet.get(0);
         String internal_wd = row.getProperty("int_wd");
         String internal_date = row.getProperty("int_date");
         String external_wd = row.getProperty("ext_wd");
         String external_date = row.getProperty("ext_date");
+
+        String rev_remarks = row.getProperty("rev_remarks");
 
         String isRevised = row.getProperty("is_revised");
         String buttonAction = row.getProperty("button_box");
@@ -44,22 +51,28 @@ public class RequestForm implements QueryHandlerInterface {
 
         row.setProperty("f_ext_wd", external_wd);
         row.setProperty("f_ext_date", external_date);
-        
+
         if (isRevised.equalsIgnoreCase("Yes") && buttonAction.equalsIgnoreCase("Submit")) {
 
-            String rev_int_wd = row.getProperty("rev_internal_wd");
-            String rev_int_date = row.getProperty("rev_int_date");
-            String rev_ext_wd = row.getProperty("rev_external_wd");
-            String rev_ext_date = row.getProperty("rev_ext_date");
+            if (!rev_remarks.equalsIgnoreCase("")) {
+                String rev_int_wd = row.getProperty("rev_internal_wd");
+                String rev_int_date = row.getProperty("rev_int_date");
+                String rev_ext_wd = row.getProperty("rev_external_wd");
+                String rev_ext_date = row.getProperty("rev_ext_date");
 
-            row.setProperty("f_int_wd", rev_int_wd);
-            row.setProperty("f_int_date", rev_int_date);
+                row.setProperty("f_int_wd", rev_int_wd);
+                row.setProperty("f_int_date", rev_int_date);
 
-            row.setProperty("f_ext_wd", rev_ext_wd);
-            row.setProperty("f_ext_date", rev_ext_date);
+                row.setProperty("f_ext_wd", rev_ext_wd);
+                row.setProperty("f_ext_date", rev_ext_date);
 
-            row.setProperty("revise_status", "Pending Approval for Manager");
-        } else if(isRevised.equalsIgnoreCase("No") && buttonAction.equalsIgnoreCase("Submit")) {
+                row.setProperty("revise_status", "Pending Approval for Manager");
+            }else {
+                Utils.showError(formData, elemId, "Revision remarks is compulsory.");
+                onCallBack.onFailure();
+            }
+
+        } else if (isRevised.equalsIgnoreCase("No") && buttonAction.equalsIgnoreCase("Submit")) {
             row.setProperty("status", "Pending Approval for TL");
         }
 
--- RssDateLoader/src/main/java/org/sunway/rssdateloader/storebinder/StoreBinderMain.java
+++ RssDateLoader/src/main/java/org/sunway/rssdateloader/storebinder/StoreBinderMain.java
@@ -5,11 +5,13 @@
  */
 package org.sunway.rssdateloader.storebinder;
 
+import org.joget.apps.app.service.AppUtil;
 import org.joget.apps.form.lib.WorkflowFormBinder;
 import org.joget.apps.form.model.Element;
 import org.joget.apps.form.model.FormData;
 import org.joget.apps.form.model.FormRow;
 import org.joget.apps.form.model.FormRowSet;
+import org.joget.apps.form.service.FormUtil;
 import org.sunway.rssdateloader.databases.QueryHandler;
 import org.sunway.rssdateloader.utilities.Utils;
 
@@ -17,9 +19,10 @@ import org.sunway.rssdateloader.utilities.Utils;
  *
  * @author kirthikan
  */
-public class StoreBinderMain extends WorkflowFormBinder {
+public class StoreBinderMain extends WorkflowFormBinder implements OnCallBack {
 
     final String pliuginName = "RSS - CustomStoreBinder";
+    private boolean setSubmit = true;
 
     public String getName() {
         return pliuginName;
@@ -47,68 +50,76 @@ public class StoreBinderMain extends WorkflowFormBinder {
 
     @Override
     public FormRowSet store(Element element, FormRowSet rowSet, FormData formData) {
-
+        FormRowSet fm = null;
         if (rowSet != null && !rowSet.isEmpty()) {
             String formId = super.getFormId(); // Take form Id to detect and perform action on Specific form
             if (formId.equalsIgnoreCase("revise_target_form")) {
-                RequestForm form = new RequestForm(rowSet);
-                form.performAction();
+                String id = FormUtil.getElementParameterName(element);
+                RequestForm form = new RequestForm(formData, rowSet, this);
+                form.performAction(id);
             }//check for manager action
             else if (formId.equalsIgnoreCase("managerViewForm")) {
                 FormRow row = rowSet.get(0);
-                String mgrAction=row.getProperty("actionButton");
-                if(mgrAction.equalsIgnoreCase("Approved"))
-                {
+                String mgrAction = row.getProperty("actionButton");
+                if (mgrAction.equalsIgnoreCase("Approved")) {
                     row.setProperty("status", "New");
                     row.setProperty("revise_status", "");
                     row.setProperty("isRevised", "No");
-                }else if(mgrAction.equalsIgnoreCase("Reject")){
+                } else if (mgrAction.equalsIgnoreCase("Reject")) {
                     row.setProperty("status", "Manager Rejected");
                 }
-             
+
             }//check for BUFinance Approval
             else if (formId.equalsIgnoreCase("tlViewForm")) {
                 FormRow row = rowSet.get(0);
-                String tlAction=row.getProperty("actionButton");
-                if(tlAction.equalsIgnoreCase("Approved"))
-                {
+                String tlAction = row.getProperty("actionButton");
+                if (tlAction.equalsIgnoreCase("Approved")) {
                     row.setProperty("status", "Pending Approval for BuFinance");
-                }else if(tlAction.equalsIgnoreCase("Reject")){
+                } else if (tlAction.equalsIgnoreCase("Reject")) {
                     row.setProperty("status", "TeamLeader Rejected");
                 }
-             
+
             }//Check for the final form status
             else if (formId.equalsIgnoreCase("buFinanceViewForm")) {
                 FormRow row = rowSet.get(0);
-                String buFinanceAction=row.getProperty("actionButton");
-                if(buFinanceAction.equalsIgnoreCase("Approved"))
-                {
+                String buFinanceAction = row.getProperty("actionButton");
+                if (buFinanceAction.equalsIgnoreCase("Approved")) {
                     row.setProperty("status", "Form Approved");
-                }else if(buFinanceAction.equalsIgnoreCase("Reject")){
+                } else if (buFinanceAction.equalsIgnoreCase("Reject")) {
                     row.setProperty("status", "BUFinance Rejected");
                 }
-             
-            }else if (formId.equalsIgnoreCase("user_change_form")){
+
+            } else if (formId.equalsIgnoreCase("user_change_form")) {
                 FormRow row = rowSet.get(0);
                 String isRevised = row.getProperty("is_revised");
                 String buttonAction = row.getProperty("button_box");
-             if (isRevised.equalsIgnoreCase("Yes") && buttonAction.equalsIgnoreCase("Submit")) {
-                String rev_int_wd = row.getProperty("rev_internal_wd");
-                String rev_int_date = row.getProperty("rev_int_date");
-                String rev_ext_wd = row.getProperty("rev_external_wd");
-                String rev_ext_date = row.getProperty("rev_ext_date");
-                row.setProperty("f_int_wd", rev_int_wd);
-                row.setProperty("f_int_date", rev_int_date);
-                row.setProperty("f_ext_wd", rev_ext_wd);
-                row.setProperty("f_ext_date", rev_ext_date);
-                row.setProperty("status", "New");
-                row.setProperty("revise_status", "Pending Approval for Manager");
-            } else if(isRevised.equalsIgnoreCase("No") && buttonAction.equalsIgnoreCase("Submit")) {
-                row.setProperty("status", "Pending Approval for TL");
-            }
+                if (isRevised.equalsIgnoreCase("Yes") && buttonAction.equalsIgnoreCase("Submit")) {
+                    String rev_int_wd = row.getProperty("rev_internal_wd");
+                    String rev_int_date = row.getProperty("rev_int_date");
+                    String rev_ext_wd = row.getProperty("rev_external_wd");
+                    String rev_ext_date = row.getProperty("rev_ext_date");
+                    row.setProperty("f_int_wd", rev_int_wd);
+                    row.setProperty("f_int_date", rev_int_date);
+                    row.setProperty("f_ext_wd", rev_ext_wd);
+                    row.setProperty("f_ext_date", rev_ext_date);
+                    row.setProperty("status", "New");
+                    row.setProperty("revise_status", "Pending Approval for Manager");
+                } else if (isRevised.equalsIgnoreCase("No") && buttonAction.equalsIgnoreCase("Submit")) {
+                    row.setProperty("status", "Pending Approval for TL");
+                }
             }
         }
-        FormRowSet fm = super.store(element, rowSet, formData);
+        if (setSubmit) {
+            fm = super.store(element, rowSet, formData);
+        }
         return fm;
+    }
+
+    public void onSuccess() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void onFailure() {
+        setSubmit = false;
     }
 }
